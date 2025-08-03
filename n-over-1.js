fc5e9f40eca2a7421c24cb6a7aa86fcca7ebe182
Hanhong Xue <macroxue@hotmail.com>
2025-08-02 22:50:22 -0400

    调整1NT开叫人在右手敌方加倍之后的叫法

--- n-over-1.js
+++ n-over-1.js
@@ -38,7 +38,7 @@ function render_text() {
                     '" href="#' + walker.currentNode.id + '">' +
                     walker.currentNode.innerHTML + '</a><br>');
     }
-    if (walker.currentNode.tagName.match(/^H[5-6]|P$/)) {
+    if (walker.currentNode.tagName.match(/^(H[5-6]|P|LI)$/)) {
       html = transform(walker.currentNode.innerHTML);
       walker.currentNode.innerHTML = html;
     }
